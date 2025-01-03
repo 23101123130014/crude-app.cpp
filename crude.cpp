af351f586c35b23237799a289f2ac90a93e26f48
23101123130014 <jacobmwansambe1@gmail.com>
2025-01-03 17:32:16 +0300

    Update crude.cpp

--- crude.cpp
+++ crude.cpp
@@ -31,7 +31,7 @@ cout<<"\n 1: create item"; //choice to create item
 cout<<"\n 2: read items"; //choice to read items
 cout<<"\n 3: update item"; //choice to update item
 cout<<"\n 4: delete item"; //choice to delete item
-cout<<"\n 5: Exit";
+cout<<"\n 5: Exit"; //choice for exiting crude application
 cout<<"choose the choice above";
 cin>>choice;
 
