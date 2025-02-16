a57d775eec562ba4324760ccc305213a9a453d95
23101123130014 <jacobmwansambe1@gmail.com>
2025-02-16 09:58:05 +0300

    Delete crude.cpp

--- crude.cpp
+++ crude.cpp
@@ -20,7 +20,6 @@ void updateItem(vector<Item>& items); // Function to update the existing product
 {
 vector<Item> items; //vector to store item
 cout<<"\n 1: create item"; //choice to create item
-cout<<"\n 2: read items"; //choice to read items
 cout<<"\n 3: update item"; //choice to update item
 cout<<"\n 4: delete item"; //choice to delete item
 cout<<"\n 5: Exit"; //choice for exiting crude application
