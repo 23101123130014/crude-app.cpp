d59fcb6e55c240ac2e6b6648fe8e03d3030d1c21
23101123130014 <jacobmwansambe1@gmail.com>
2025-02-16 09:58:02 +0300

    Delete crude.cpp

--- crude.cpp
+++ crude.cpp
@@ -19,7 +19,6 @@ void readItems(const vector<Item>& items); // Function to read and display all i
 void updateItem(vector<Item>& items); // Function to update the existing product
 {
 vector<Item> items; //vector to store item
-do
 cout<<"\n 1: create item"; //choice to create item
 cout<<"\n 2: read items"; //choice to read items
 cout<<"\n 3: update item"; //choice to update item
