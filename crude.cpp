d09a37836f285b8d0cba37bf5ecd2095cda385a0
23101123130014 <jacobmwansambe1@gmail.com>
2025-02-16 10:01:47 +0300

    Delete crude.cpp

--- crude.cpp
+++ crude.cpp
@@ -23,7 +23,6 @@ cout<<"\n 1: create item"; //choice to create item
 	}
 //function to read items
 		else{
-			for(const auto& item : items)
 			item.display(); //it display the items found in the vector
 			
 						}
