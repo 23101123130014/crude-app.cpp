3ca2df7052487382e216b6f67276789ece2420fd
23101123130014 <jacobmwansambe1@gmail.com>
2025-02-16 09:59:18 +0300

    Delete crude.cpp

--- crude.cpp
+++ crude.cpp
@@ -22,7 +22,6 @@ vector<Item> items; //vector to store item
 cout<<"\n 1: create item"; //choice to create item
 cout<<"\n 3: update item"; //choice to update item
 				break;
-					case 4:
 						deleteItem(items); //case which calls funtion to delete item
 						case 5:
 							cout<<"\n exting crude application"; //case for exiting the application
