2583530e05717e37447dfbbb846ac77d5cf02e05
23101123130014 <jacobmwansambe1@gmail.com>
2025-02-16 10:02:22 +0300

    Delete crude.cpp

--- crude.cpp
+++ crude.cpp
@@ -22,7 +22,6 @@ vector<Item> items; //vector to store item
 cout<<"\n 1: create item"; //choice to create item
 	}
 //function to read items
-
 void updateItem(vector<Item>& items)
 {
 	int id; //variable to hold the id of the item to update
