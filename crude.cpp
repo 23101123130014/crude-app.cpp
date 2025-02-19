1caca4db1e89cc6b6b0c9cd6b8af761ddd47b5f7
23101123130014 <jacobmwansambe1@gmail.com>
2025-02-19 11:16:59 +0300

    Delete crude.cpp

--- crude.cpp
+++ crude.cpp
@@ -1,5 +1,4 @@
 #include <string>  // enable the use of std:string
-#include <vector>  // Include the vector library for use of std::vector
 using namespace std;  // Use the standard namespace to avoid prefixing std::
 /
 struct Item
